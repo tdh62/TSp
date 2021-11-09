f8f77724e2bc47e1e9258a46bbb3d2690a40f191
tdh <821028463@qq.com>
2021-11-09 11:16:15 +0800

    初始化
    首次上传
    
    COS 上传腾讯云供能测试

---
+++ global/settings.js
@@ -0,0 +1,6 @@
+const upload_timeout = 30; // 上传云存储超时时间
+
+//---------------------------测试用---------------------------------
+// 腾讯云 COS
+const COS_URL = "tsp-1252066102.cos.ap-guangzhou.myqcloud.com";
+
