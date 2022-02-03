081103b2221023901e8e593909a5e2e3f9293ebd
tdh <tdh@tdh6.top>
2022-02-03 20:47:19 +0800

    调整结构，本地存储，Base64编解码处理

---
+++ component/storage/local.js
@@ -0,0 +1,181 @@
+/**
+ * 存储模块
+ * Copyright @tdh
+ */
+
+// 本地长期存储
+/**
+ * 保存数据到本地
+ * @param s 数据名称
+ * @param k 数据内容
+ */
+function save_to_local(s,k){
+    localStorage.setItem(s,k)
+}
+
+/**
+ * 从本地读取数据
+ * @param s 数据名称
+ * @returns {string} 数据内容
+ */
+function read_from_local(s){
+    return localStorage.getItem(s)
+}
+
+/**
+ * 从本地删除数据
+ * @param s 数据名称
+ */
+function delete_from_local(s){
+    localStorage.removeItem(s)
+}
+
+/**
+ * 清空本地存储的数据
+ */
+function clear_local_saves(){
+    localStorage.clear()
+}
+
+// 本地回话存储
+/**
+ * 保存数据到回话
+ * @param s 数据名称
+ * @param k 数据内容
+ */
+function save_to_session(s,k){
+    sessionStorage.setItem(s,k)
+}
+
+/**
+ * 从会话读取数据
+ * @param s 数据名称
+ * @returns {string} 数据内容
+ */
+function read_from_session(s){
+    return sessionStorage.getItem(s)
+}
+
+/**
+ * 从会话删除数据
+ * @param s 数据名称
+ */
+function delete_from_session(s){
+    sessionStorage.removeItem(s)
+}
+
+/**
+ * 清空会话存储的数据
+ */
+function clear_session_saves(){
+    sessionStorage.clear()
+}
+
+// Cookies
+// From w3c
+
+/**
+ * 设置 Cookie
+ * @param c_name 名称
+ * @param c_value 内容
+ * @param ex_days 过期时间（天）
+ */
+function save_to_cookie(c_name, c_value, ex_days = 30) {
+    const d = new Date();
+    d.setTime(d.getTime() + (ex_days*24*60*60*1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = c_name + "=" + c_value + "; " + expires;
+}
+
+/**
+ * 获取 Cookie 内容
+ * @param c_name 名称
+ * @returns {string} Cookie 内容
+ */
+function read_from_cookie(c_name) {
+    const name = c_name + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0; i<ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0)===' ') c = c.substring(1);
+        if (c.indexOf(name) !== -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+/**
+ * 从 Cookie 中删除数据
+ * @param name 数据名称
+ */
+function delete_from_cookie(name) {
+    save_to_cookie(name, "", -1);
+}
+
+/**
+ * 清空所有本地存储
+ * Cookies 并不会被清空
+ */
+function clear_all_saves(){
+    clear_local_saves()
+    clear_session_saves()
+}
+
+// 常用操作
+/**
+ * 存储数据至本地
+ *
+ * 存储将优先进行字符串转换
+ *
+ * @param s 数据名
+ * @param k 数据内容
+ * @param target 存储目标（local,cookie,session）
+ * @param formats 编码格式（string,base64）
+ */
+function saves(s,k,target="session",formats = "string"){
+    if (typeof k === "object"){
+        // 转换对象到 JSON 字符串
+        k = json_to_str(k)
+    }
+    if (formats === "base64"){
+        // 处理 Base64 编码
+        k = b64encode(k)
+    }
+    if (target === "session"){
+        save_to_session(s,k)
+    }else if (target === "local"){
+        save_to_local(s,k)
+    }else if (target === "cookie"){
+        save_to_cookie(s,k)
+    }else{
+        console.error("!!!在使用本地存储时错误地指定了目标!!!")
+    }
+}
+
+/**
+ * 从本地存储读取数据
+ * @param s 数据名
+ * @param target 存储目标（local,cookie,session）
+ * @param formats 编码格式（string,base64）
+ * @return {object} 解析结果
+ */
+function reads(s,target="session",formats = "string"){
+    let k = ""
+    if (target === "session"){
+        k = read_from_session(s)
+    }else if (target === "local"){
+        k = read_from_local(s)
+    }else if (target === "cookie"){
+        k = read_from_cookie(s)
+    }else{
+        console.error("!!!在使用本地存储时错误地指定了目标!!!")
+    }
+    if (formats === "base64"){
+        // 处理 Base64 编码
+        k = b64dncode(k)
+    }
+    if (typeof k === "object"){
+        // 转换对象到 JSON 字符串
+        k = str_to_json(k)
+    }
+    return k
+}
---
+++ global/js/common.js
@@ -0,0 +1,73 @@
+/**
+ * 通用功能
+ * Copyright @tdh
+ *
+ * 通用模块应最先被导入
+ */
+
+/**
+ * 解析 JSON 字符串
+ * @param s
+ * @returns {any}
+ */
+function str_to_json(s){
+    return eval(s)
+}
+
+/**
+ * 对象转为 JSON 字符串
+ * @param j
+ * @returns {string}
+ */
+function json_to_str(j){
+    if (typeof(j) === "object"){
+        return JSON.stringify(j)
+    }else if(typeof(j) === "string"){
+        return j
+    }else{
+        return ""
+    }
+}
+
+/**
+ * Base64 编码
+ * @param s 字符串
+ * @returns {string} 编码结果
+ */
+function b64encode(s){
+    return window.btoa(s)
+}
+
+/**
+ * Base64 解码
+ * @param s 字符串
+ * @returns {string} 解码结果
+ */
+function b64decode(s){
+    return window.atob(s)
+}
+
+/**
+ * 文件 Base64 编码
+ * @param f 待编码文件
+ */
+function file_b64encode(f){
+    return (new FileReader()).readAsDataURL(f)
+}
+
+
+/**
+ * binaryBase64 计算
+ *
+ * // From tencent cloud API documents
+ * @param str 待编码数据
+ * @returns {string} Base64 编码后的数据
+ */
+const binaryBase64 = function (str) {
+    let i, len, char, res = '';
+    for (i = 0, len = str.length / 2; i < len; i++) {
+        char = parseInt(str[i * 2] + str[i * 2 + 1], 16);
+        res += String.fromCharCode(char);
+    }
+    return btoa(res);
+};
