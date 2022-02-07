90f906f538dc19277cb6fd3a2006ebe39f8923de
tdh <tdh@tdh6.top>
2022-02-07 22:18:40 +0800

    初始化和页眉页脚测试

---
+++ js/headerAndFooter.js
@@ -0,0 +1,8 @@
+
+
+axios.get("/footer.html").then((r)=>{
+        document.getElementById("site-footer").innerHTML = r.data
+}).catch((e)=>{
+    console.error(e)
+    document.getElementById("site-footer").innerHTML = "页脚尚未设置，请将其上传至 /footer.html"
+})
