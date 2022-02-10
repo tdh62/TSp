2b5e668ea20ea98f00c8ecfacebcf387ed50d6c7
tdh <tdh@tdh6.top>
2022-02-10 19:40:17 +0800

    关于页面

---
+++ js/aboutmain.js
@@ -0,0 +1,48 @@
+
+mainapp = Vue.createApp({
+    data(){
+        return {
+            "loading":true,
+        }
+    },
+    computed:{
+        "index_image"(){
+            return this.banner_list[Math.round(Math.random()*(this.banner_list.length-1))]
+        },
+        "font_size_fix"(){
+            const ft = read_from_local("font_size_fix")
+            return ft?ft:"1em"
+        }
+    },
+    mounted(){
+        // 从 u 获取MarkDown 并显示
+        reads_remote("/about.md",(r)=>{
+            MDView = editormd.markdownToHTML("index-main", {
+                    markdown: r,
+                    // htmlDecode      : true,       // 开启 HTML 标签解析，为了安全性，默认不开启
+                    htmlDecode: "style,script,iframe",  // you can filter tags decode
+                    // toc             : true,
+                    // tocContainer    : "#article_aside", // 自定义 ToC 容器层
+                    // tocm: true,    // Using [TOCM]
+                    emoji: true,
+                    taskList: true,
+                    tex: true,  // 默认不解析
+                    flowChart: true,  // 默认不解析
+                    sequenceDiagram: true,  // 默认不解析
+                }
+            )
+            MDView.appendMarkdown("\n\n")
+            reads_remote("/README.md",(r)=>{
+                MDView.appendMarkdown(r)
+            },false,(e)=>{console.log(e);})
+        },false,(e)=>{console.log(e);alert("加载失败，请刷新重试")})
+
+        reads_remote("/settings/banner.txt",(r)=>{
+            if (r.length > 0 ){
+                this.banner_list = r
+            }
+        },true,(e)=>{console.error(e)})
+        this.loading = false
+    }
+})
+mainapp.use(ElementPlus)
